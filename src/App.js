baa124017b5e435813dd342b56b5f3fd9f2ed0f4
Sahil Gupta <sahilofficial17@gmail.com>
2023-11-18 14:10:50 -0800

    Initialized Next app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Dashboard from './homepage';
+import './css/App.css';
+import NavBar from './components/navbar';
+
+
+
+const App = () => {
+
+
+return (
+  <>
+  <div>
+    <NavBar/>
+    <Dashboard/>
+  </div>
+   </>
+  );
+}
+
+export default App;
